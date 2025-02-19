68af223364ba9c7165586a5733603549f13f5a58
Jafarbek Yusupov <j.yusupov@newuu.uz>
2025-02-20 02:24:28 +0500

    ...

--- AirVehicle.h
+++ AirVehicle.h
@@ -1,1 +1,14 @@
+#infdef AIRVEHICLE_H
+#define AIRVEHICLE_H
+#include "Vehicle.h"
 
+class AirVehicle : public Vehicle{
+protected: int maxAlt;
+public:
+    AirVehicle(std::string brand, int year, int maxAlt) : Vehicle(brand, year), maxAlt(maxAlt) {}
+    void getInfo() const override{
+        cout << "AirVehicle class:: Max altitude: " << maxAlt << " meters" << std::endl << "Vehicle class:: Brand: " << brand << std::endl << "Vehicle class:: Year: " << year << std::endl;
+    }
+};
+
+#endif
