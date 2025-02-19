2aaea4a942aabd54cbe07591a64235805ff6e13a
Jafarbek Yusupov <j.yusupov@newuu.uz>
2025-02-20 02:47:54 +0500

    ...

--- Boat.h
+++ Boat.h
@@ -7,7 +7,7 @@ protected: double length;
 public:
     Boat(std::string brand, int year, std::string vesselType, double length) : WaterVehicle(brand, year, veseelType), length(length) {}
     void getInfo() const override {
-        cout << "Boat class:: Length: " << length << " meters" << std::endl;
+        std::cout << "Boat class:: Length: " << length << " meters" << std::endl;
         WaterVehicle::getInfo(); 
     }
 };
