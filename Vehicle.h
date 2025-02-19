dffa006c6529dfd3f6342d7455a17e5d2a1e74dc
Jafarbek Yusupov <j.yusupov@newuu.uz>
2025-02-19 11:08:09 +0500

    ...

--- Vehicle.h
+++ Vehicle.h
@@ -1,1 +1,15 @@
+// #ifndef VEHICLE_H
+// #define VEHICLE_H
 
+#include <iostream>
+#include <string>
+
+class Vehicle{
+protected: std::string brand; int year;
+public:
+    Vehicle(const std::string& brand, int year) : brand(brand), year(year){}
+    virtual void getInfo() const = 0;
+    virtual ~Vehicle() = default;
+};
+
+// #endif
