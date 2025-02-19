c505ee98bfc047f350d63a43a7d21781fae7b6f6
Jafarbek Yusupov <j.yusupov@newuu.uz>
2025-02-20 02:46:54 +0500

    ...

--- Submarine.h
+++ Submarine.h
@@ -1,1 +1,15 @@
+#ifndef SUBMARINE_H
+#define SUBMARINE_H
+#include "WaterVehicle.h"
 
+class Submarine : public WaterVehicle{
+protected: int maxDepth;
+public:
+    Submarine(std::string brand, int year, std::string vesselType) : WaterVehicle(brand, year, vesselType), maxDepth(maxDepth) {}
+    void getInfo() const override {
+        std::cout << "Submarine class:: Max depth: " << maxDepth << " meters" << std::endl;
+        WaterVehicle::getInfo();
+    }
+};
+
+#endif
