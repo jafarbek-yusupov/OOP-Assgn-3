31721d3d5083425f76154c13cc793fce02691a04
Jafarbek Yusupov <j.yusupov@newuu.uz>
2025-02-20 02:09:45 +0500

    ...

--- Truck.h
+++ Truck.h
@@ -1,1 +1,15 @@
+#ifndef TRUCK_H
+#define TRUCK_H
+#include "LandVehicle.h"
 
+class Truck : public LandVehicle {
+protected: double loadCapacity;
+public:
+    Truck(std::string brand, int year, int wheels, double loadCapacity): LandVehicle(brand, year, wheels), loadCapacity(loadCapacity) {}
+    void getInfo() const override {
+        std::cout << "Truck class:: Load capacity: " << loadCapacity << std::endl;
+        LandVehicle::getInfo();
+    }
+};
+
+#endif
