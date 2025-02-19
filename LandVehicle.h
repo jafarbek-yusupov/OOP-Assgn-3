e57d192695ce62127d5a255799b67653a106de00
Jafarbek Yusupov <j.yusupov@newuu.uz>
2025-02-20 03:17:19 +0500

    _-_

--- LandVehicle.h
+++ LandVehicle.h
@@ -6,8 +6,8 @@ class LandVehicle : public Vehicle{
 protected: int wheels;
 public: 
     LandVehicle(std::string brand, int year, int wheels) : Vehicle(brand, year), wheels(wheels){}
-    void getInfo() const override {
-        std::cout << "LandVehicle class:: Number of wheels:" << wheels << std::endl;
+    void getInfo() const override{
+        std::cout << "LandVehicle class:: Number of wheels: " << wheels << std::endl;
         std::cout << "Vehicle class:: Brand: " << brand << std::endl << "Vehicle class:: Year: " << year << std::endl;
     }
 };
