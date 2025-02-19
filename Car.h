74ad566a1ed3fac07a22c289390f17f066a9802c
Jafarbek Yusupov <j.yusupov@newuu.uz>
2025-02-20 03:28:39 +0500

    _-_

--- Car.h
+++ Car.h
@@ -5,11 +5,11 @@
 class Car : public LandVehicle{
 private: int numDoors;
 public:
-    Car(std::string brand, int year, int wheels, int numDoors) : LandVehicle(brand, year, wheels), numDoors(numDoors) {}
-    void getInfo() const override{ 
-        std::cout << "Car class:: Number of doors: " << numDoors << std::endl; 
-        LandVehicle::getInfo();
-    } 
+Car(std::string brand, int year, int wheels, int numDoors) : LandVehicle(brand, year, wheels), numDoors(numDoors) {}
+void getInfo() const override{ 
+    std::cout << "Car class:: Number of doors: " << numDoors << std::endl; 
+    LandVehicle::getInfo();
+} 
 };
 
 #endif
