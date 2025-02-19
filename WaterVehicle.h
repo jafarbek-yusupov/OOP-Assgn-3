0b7d09f7c0117c9acc5e4a0c1d1b99cdede08ca3
Jafarbek Yusupov <j.yusupov@newuu.uz>
2025-02-20 02:33:45 +0500

    ...

--- WaterVehicle.h
+++ WaterVehicle.h
@@ -1,1 +1,14 @@
+#ifndef WATERVEHICLE_H
+#define WATERVEHICLE_H
+#include "Vehicle.h"
 
+class WaterVehicle : public Vehicle {
+protected: std::string vesselType;
+public:
+    WaterVehicle(std::string brand, int year, std::string vesselType) : Vehicle(brand, year), vesselType(vesselType) {}
+    void getInfo() const override{
+        cout << "WaterVehicle class:: Vessel type: " << vesselType << std::endl << "Vehicle class:: Brand: " << brand << std::endl << "Vehicle class:: Year: " << year << std::endl;
+    }
+};
+
+#endif
