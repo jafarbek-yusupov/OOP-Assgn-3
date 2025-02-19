9020236d7081f139864398340e2f93ba42cf522e
Jafarbek Yusupov <j.yusupov@newuu.uz>
2025-02-20 02:29:14 +0500

    ...

--- Helicopter.h
+++ Helicopter.h
@@ -1,1 +1,15 @@
+#ifndef HELICOPTER_H
+#define HELICOPTER_H
+#inclue "AirVehicle.h"
 
+class Helicopter : public AirVehicle{
+protected: int rotorCount;
+public: 
+    Helicopter(std::string brand, int year, int maxAlt, int rotorCount) : AirVehicle(brand, year, maxAlt), rotorCount(rotorCount) {}
+    void getInfo() const override{
+        cout << "Helicopter class:: Rotor count: " << rotorCount << std::endl;
+        AirVehicle::getInfo();
+    }
+};
+
+#endif
