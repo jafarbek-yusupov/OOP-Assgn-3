7bfe11562e87799886c9b8f7a346e683a840fb1e
Jafarbek Yusupov <j.yusupov@newuu.uz>
2025-02-20 03:28:52 +0500

    _-_

--- Helicopter.h
+++ Helicopter.h
@@ -5,11 +5,11 @@
 class Helicopter : public AirVehicle{
 private: int rotorCount;
 public:
-    Helicopter(std::string brand, int year, int maxAlt, int rotorCount) : AirVehicle(brand, year, maxAlt), rotorCount(rotorCount) {}
-    void getInfo() const override{
-        std::cout << "Helicopter class:: Rotor count: " << rotorCount << std::endl;
-        AirVehicle::getInfo();
-    }
+Helicopter(std::string brand, int year, int maxAlt, int rotorCount) : AirVehicle(brand, year, maxAlt), rotorCount(rotorCount) {}
+void getInfo() const override{
+    std::cout << "Helicopter class:: Rotor count: " << rotorCount << std::endl;
+    AirVehicle::getInfo();
+}
 };
 
 #endif
