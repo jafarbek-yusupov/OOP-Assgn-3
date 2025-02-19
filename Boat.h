c6c55859195d801680a1183a559ca932d0f772c5
Jafarbek Yusupov <j.yusupov@newuu.uz>
2025-02-20 02:42:18 +0500

    ..

--- Boat.h
+++ Boat.h
@@ -1,1 +1,15 @@
+#ifndef BOAT_H
+#define BOAT_H
+#include "WaterVehicle.h"
 
+class Boat : public WaterVehicle{
+protected: double length;
+public:
+    Boat(std::string brand, int year, std::string vesselType, double length) : WaterVehicle(brand, year, veseelType), length(length) {}
+    void getInfo() const override{
+        cout << "Boat class:: Length: " << length << " meters" << std::endl;
+        WaterVehicle::getInfo(); 
+    }
+};
+
+#endif
