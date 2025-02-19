561029d1148581801341eed4e2748406720d2efa
Jafarbek Yusupov <j.yusupov@newuu.uz>
2025-02-20 03:29:38 +0500

    _-_

--- Truck.h
+++ Truck.h
@@ -5,11 +5,11 @@
 class Truck : public LandVehicle{
 private: double loadCapacity;
 public:
-    Truck(std::string brand, int year, int wheels, double loadCapacity): LandVehicle(brand, year, wheels), loadCapacity(loadCapacity) {}
-    void getInfo() const override{
-        std::cout << "Truck class:: Load capacity: " << loadCapacity << " tons" << std::endl;
-        LandVehicle::getInfo();
-    }
+Truck(std::string brand, int year, int wheels, double loadCapacity): LandVehicle(brand, year, wheels), loadCapacity(loadCapacity) {}
+void getInfo() const override{
+    std::cout << "Truck class:: Load capacity: " << loadCapacity << " tons" << std::endl;
+    LandVehicle::getInfo();
+}
 };
 
 #endif
